e46d2a6ea9a74dfd78d3f7a3a46073643083b82d
yexiaoqi01 <15811267352@163.com>
2017-10-09 09:54:01 +0800

    完成css并压缩

--- gulpFile.js
+++ gulpFile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var connect = require('gulp-connect');
+var mincss = require('gulp-clean-css');
 
 gulp.task('minjs', function () {
     gulp.src(['./delete.js', './againD.js'])
@@ -8,4 +10,9 @@ gulp.task('minjs', function () {
         .pipe(uglify())
         .pipe(gulp.dest('./minjs/'))
 })
-gulp.task('default', ['minjs']);
+gulp.task('mincss', function () {
+    gulp.src('./style.css')
+        .pipe(mincss())
+        .pipe(gulp.dest('./mincss/'))
+})
+gulp.task('default', ['minjs', 'mincss']);
